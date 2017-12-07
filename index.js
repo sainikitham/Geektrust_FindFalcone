8f2eea96559f51a331f081d9e8687a22c3b2ab02
Nikitha <a614343@fmr.com>
2017-12-07 18:33:49 +0530

    post req modification

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.config(function($routeProvider) {
         controller : "myCtrl"
     });
 });
-app.controller('myCtrl', function($scope, $http, $rootScope) {
+app.controller('myCtrl', function($scope, $http) {
     $http.get("https://findfalcone.herokuapp.com/planets")
     .then(function(response) {
         $scope.planets = response.data;
@@ -19,7 +19,8 @@ app.controller('myCtrl', function($scope, $http, $rootScope) {
     .then(function(response) {
         $scope.vehicles = response.data;
     });
-   $scope.update = function(id,bool){
+   $scope.update = function(id,bool,item){
+
      if (bool == 'vehicles1') {
        $scope.vehicles1  = true;
      }
@@ -32,12 +33,37 @@ app.controller('myCtrl', function($scope, $http, $rootScope) {
      if (bool == 'vehicles4') {
        $scope.vehicles4  = true;
      }
-     var distance = document.getElementById(id).value;
+      if (item == 'Donlon') {
+        var distance = 100;
+      }else if (item == "Enchai") {
+        var distance = 200;
+      }
+      else if (item == "Jebing") {
+        var distance = 300;
+      }else if (item == "Sapir") {
+        var distance = 400;
+      }
+      else if (item == "Lerbin") {
+        var distance = 500;
+      }else if (item == "Pingasor") {
+        var distance = 600;
+      }
+     //var distance = document.getElementById(id).value;
      $scope.distance = distance;
  };
-   $scope.start = function(id,disable) {
+
+   $scope.start = function(id,disable,flash) {
     document.getElementById(disable).disabled = true;
     //document.getElementById(id.vehicle.name).checked = true;
+    if (flash == 'vehicles1') {
+      $scope.vehicles1 = id.vehicle.name;
+    }else if (flash == 'vehicles2') {
+      $scope.vehicles2 = id.vehicle.name;
+    }else if (flash == 'vehicles3') {
+      $scope.vehicles3 = id.vehicle.name;
+    }else if (flash == 'vehicles4') {
+      $scope.vehicles4 = id.vehicle.name;
+    }
     var x = id.vehicle.speed;
     var y = $scope.distance;
     var a = eval("y / x") ;
@@ -57,10 +83,6 @@ app.controller('myCtrl', function($scope, $http, $rootScope) {
    }
    $scope.getDetails = function() {
      var time = localStorage.getItem('time');
-     $scope.item = "Donlon";
-     $scope.item1 = "Enchai";
-     $scope.item2 = "Sapir";
-     $scope.token = "bCQfJqPMTyegYCwedccNOSEmderKMpLt";
      var req = {
      method: 'POST',
      url: 'http://findfalcone.herokuapp.com/token',
@@ -74,6 +96,22 @@ app.controller('myCtrl', function($scope, $http, $rootScope) {
          find($scope.token)
      });
      function find(val){
+       //planets
+       var plane = $scope.item;
+       var plane1 = $scope.item1;
+       var plane2 = $scope.item2;
+       var plane3 = $scope.item3;
+       var cars = new Array;
+       cars.push(plane);cars.push(plane1);cars.push(plane2);cars.push(plane3);
+       console.log(cars);
+       //vehicles
+       var blah = $scope.vehicles1;
+       var blah1 = $scope.vehicles2;
+       var blah2 = $scope.vehicles3;
+       var blah3 = $scope.vehicles4;
+       var bikes = new Array;
+       bikes.push(blah);bikes.push(blah1);bikes.push(blah2);bikes.push(blah3);
+       console.log(bikes);
        var req = {
        method: 'POST',
        url: 'http://findfalcone.herokuapp.com/find',
@@ -83,21 +121,12 @@ app.controller('myCtrl', function($scope, $http, $rootScope) {
        },
        data: {
            "token": "tjqHYqwfTCGfksrSwzoRASRywZrqyqZk",
-           "planet_names" : [
-           	$scope.item,
-           	$scope.item1,
-           	$scope.item2,
-           	"Pingasor"
-           ],
-           "vehicle_names" : [
-           	"Space pod",
-           	"Space rocket",
-           	"Space ship",
-           	"Space ship"
-           ]
+           "planet_names" : cars,
+           "vehicle_names" : bikes
        }
        }
        $http(req).then(function(response){
+         console.log(response.data);
          if (response.data.status == "success") {
            document.getElementById("result").innerHTML = `<div style="text-align: center;padding:5px;">
              <h3>Success!!
@@ -109,7 +138,7 @@ app.controller('myCtrl', function($scope, $http, $rootScope) {
            </div>`;
          }
          else {
-           document.getElementById("result").innerHTML = `<h2 class="text-center">Failure!! Try Again....</h2>
+           document.getElementById("result").innerHTML = `<h2 class="text-center">Failed on Finding Falcone.</h2>
            <a href="#!/" class="btn btn-large btn-default" style="margin-left: 43%;margin-top: 9%;">Start Again</a>`;
          }
        });

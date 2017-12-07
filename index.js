6142ee8cd6fa11c979792f84ed478acff20383b7
Nikitha <a614343@fmr.com>
2017-12-07 15:02:50 +0530

    added routing

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
-    //$scope.carname = "Volvo";
+var app = angular.module('myApp', ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "templates/start.html",
+        controller : "myCtrl"
+    })
+    .when("/find", {
+        templateUrl : "templates/final.html",
+        controller : "myCtrl"
+    });
+});
+app.controller('myCtrl', function($scope, $http, $rootScope) {
     $http.get("https://findfalcone.herokuapp.com/planets")
     .then(function(response) {
         $scope.planets = response.data;
-        console.log($scope.planets);
     });
     $http.get("https://findfalcone.herokuapp.com/vehicles")
     .then(function(response) {
         $scope.vehicles = response.data;
-        console.log($scope.vehicles);
     });
-
    $scope.update = function(id,bool){
      if (bool == 'vehicles1') {
        $scope.vehicles1  = true;
@@ -27,7 +34,6 @@ app.controller('myCtrl', function($scope, $http) {
      }
      var distance = document.getElementById(id).value;
      $scope.distance = distance;
-     console.log($scope.distance);
  };
    $scope.start = function(id,disable) {
     document.getElementById(disable).disabled = true;
@@ -36,7 +42,6 @@ app.controller('myCtrl', function($scope, $http) {
     var y = $scope.distance;
     var a = eval("y / x") ;
     var res = a ;
-    console.log("time is "+ res);
     if (document.getElementById("time").innerHTML == "") {
       document.getElementById("time").innerHTML = res;
     }
@@ -44,10 +49,70 @@ app.controller('myCtrl', function($scope, $http) {
       var x = document.getElementById("time").innerHTML;
       var x = +x + +res;
       document.getElementById("time").innerHTML = x;
+      localStorage.setItem('time', JSON.stringify(x));
     }
     if (disable == 'myFieldset3') {
       document.getElementById('btn').disabled = false;
     }
-
+   }
+   $scope.getDetails = function() {
+     var time = localStorage.getItem('time');
+     $scope.item = "Donlon";
+     $scope.item1 = "Enchai";
+     $scope.item2 = "Sapir";
+     $scope.token = "bCQfJqPMTyegYCwedccNOSEmderKMpLt";
+     var req = {
+     method: 'POST',
+     url: 'http://findfalcone.herokuapp.com/token',
+     headers: {
+       'Accept' : 'application/json'
+     },
+     data: { }
+     }
+     $http(req).then(function(response){
+         $scope.token = JSON.stringify(response.data.token);
+         find($scope.token)
+     });
+     function find(val){
+       var req = {
+       method: 'POST',
+       url: 'http://findfalcone.herokuapp.com/find',
+       headers: {
+         'Accept' : 'application/json',
+         'Content-Type' :'application/json'
+       },
+       data: {
+           "token": "tjqHYqwfTCGfksrSwzoRASRywZrqyqZk",
+           "planet_names" : [
+           	$scope.item,
+           	$scope.item1,
+           	$scope.item2,
+           	"Pingasor"
+           ],
+           "vehicle_names" : [
+           	"Space pod",
+           	"Space rocket",
+           	"Space ship",
+           	"Space ship"
+           ]
+       }
+       }
+       $http(req).then(function(response){
+         if (response.data.status == "success") {
+           document.getElementById("result").innerHTML = `<div style="text-align: center;padding:5px;">
+             <h3>Success!!
+               Congratulation on Finding Falcone.
+               King Shan is mighty pleased.</h3>
+               <h3>Time taken: ${time}</h3>
+               <h3>Planet found: ${response.data.planet_name}</h3>
+             <a href="#!/" class="btn btn-large btn-default">Start Again</a>
+           </div>`;
+         }
+         else {
+           document.getElementById("result").innerHTML = `<h2 class="text-center">Failure!! Try Again....</h2>
+           <a href="#!/" class="btn btn-large btn-default" style="margin-left: 43%;margin-top: 9%;">Start Again</a>`;
+         }
+       });
+     }
    }
 });
